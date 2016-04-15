ed6d26069f5593c296b0061316cddd481303d7cf
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-04-15 10:15:50 +0200

    Add update_buffer_untyped

--- vulkano/src/command_buffer/sys/clear.rs
+++ vulkano/src/command_buffer/sys/clear.rs
@@ -9,6 +9,7 @@
 
 use std::error;
 use std::fmt;
+use std::mem;
 use std::sync::Arc;
 use smallvec::SmallVec;
 
@@ -71,6 +72,59 @@ impl UnsafeCommandBufferBuilder {
             Ok(self)
         }
     }
+
+    /// Adds a command that writes the content of a buffer.
+    ///
+    /// The size of the buffer slice is what is taken into account when determining the size. If
+    /// `data` is larger than the slice, then the remaining will be ignored. If `data` is smaller,
+    /// an error is returned.
+    ///
+    /// If the size of the slice is 0, no command is added.
+    ///
+    /// # Panic
+    ///
+    /// - Panicks if the buffer was not allocated with the same device as this command buffer.
+    ///
+    pub fn update_buffer_untyped<'a, S, T: ?Sized, B, D: ?Sized>(mut self, buffer: S, data: &D)
+                                                                 -> Result<UnsafeCommandBufferBuilder,
+                                                                           BufferUpdateError>
+        where S: Into<BufferSlice<'a, T, B>>,
+              B: Buffer + Send + Sync + 'static
+    {
+        unsafe {
+            let buffer = buffer.into();
+
+            // Performing checks.
+            if self.within_render_pass { return Err(BufferUpdateError::ForbiddenWithinRenderPass); }
+            assert_eq!(buffer.buffer().inner_buffer().device().internal_object(),
+                       self.pool.device().internal_object());
+            if !buffer.buffer().inner_buffer().usage_transfer_src() {
+                return Err(BufferUpdateError::WrongUsageFlag);
+            }
+            if (buffer.offset() % 4) != 0 || (buffer.size() % 4) != 0 {
+                return Err(BufferUpdateError::WrongAlignment);
+            }
+            if buffer.size() == 0 { return Ok(self); }
+            if buffer.size() >= 0x10000 { return Err(BufferUpdateError::RegionTooLarge); }
+            if mem::size_of_val(data) < buffer.size() {
+                return Err(BufferUpdateError::DataTooSmall);
+            }
+
+            // Adding the command.
+            self.keep_alive.push(buffer.buffer().clone());
+
+            {
+                let vk = self.device.pointers();
+                let cmd = self.cmd.clone().unwrap();
+                vk.CmdUpdateBuffer(cmd, buffer.buffer().inner_buffer().internal_object(),
+                                   buffer.offset() as vk::DeviceSize,
+                                   (buffer.size() / 4) as vk::DeviceSize,
+                                   data as *const D as *const u32);
+            }
+
+            Ok(self)
+        }
+    }
 }
 
 /// A copy between two buffers.
@@ -90,4 +144,12 @@ error_ty!{BufferFillError => "Error that can happen when filling a buffer.",
                                   support this operation",
     WrongUsageFlag => "one of the buffers doesn't have the correct usage flag",
     WrongAlignment => "the offset and size must be multiples of 4",
+}
+
+error_ty!{BufferUpdateError => "Error that can happen when updating a buffer.",
+    ForbiddenWithinRenderPass => "can't copy buffers from within a render pass",
+    WrongUsageFlag => "one of the buffers doesn't have the correct usage flag",
+    WrongAlignment => "the offset and size must be multiples of 4",
+    RegionTooLarge => "the size of the region exceeds the allowed limits",
+    DataTooSmall => "the data is smaller than the region to copy to",
 }
