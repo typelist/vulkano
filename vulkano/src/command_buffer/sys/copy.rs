d08bc478eca604f79e39527a314771a96c8ccce4
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-04-23 20:01:12 +0200

    Use a new design for command buffers commands

--- vulkano/src/command_buffer/sys/copy.rs
+++ vulkano/src/command_buffer/sys/copy.rs
@@ -30,6 +30,7 @@ use buffer::traits::AccessRange as BufferAccessRange;
 use command_buffer::CommandBufferPool;
 use command_buffer::DrawIndirectCommand;
 use command_buffer::DynamicState;
+use command_buffer::sys::KeepAlive;
 use command_buffer::sys::UnsafeCommandBufferBuilder;
 use descriptor::descriptor_set::DescriptorSetsCollection;
 use descriptor::PipelineLayout;
@@ -65,106 +66,130 @@ use VulkanPointers;
 use check_errors;
 use vk;
 
-impl UnsafeCommandBufferBuilder {
+/// Prototype for a command that copies data between buffers.
+pub struct BufferCopyCommand {
+    keep_alive_src: Arc<KeepAlive + 'static>,
+    keep_alive_dest: Arc<KeepAlive + 'static>,
+    device: vk::Device,
+    internal_src: vk::Buffer,
+    internal_dest: vk::Buffer,
+    regions: SmallVec<[vk::BufferCopy; 4]>,
+}
+
+impl BufferCopyCommand {
     /// Adds a command that copies regions between a source and a destination buffer. Does not
-    /// check the type of the content, contrary to `copy_buffer`.
+    /// check the type of the content.
     ///
     /// Regions whose size is 0 are automatically ignored. If no region was passed or if all
     /// regions have a size of 0, then no command is added to the command buffer.
     ///
     /// # Panic
     ///
-    /// - Panicks if one of the buffers was not allocated with the same device as this command
-    ///   buffer.
+    /// - Panicks if the two the buffers were not allocated with the same device.
     ///
-    pub fn copy_buffer_untyped<Bs, Bd, I>(mut self, src: &Arc<Bs>, dest: &Arc<Bd>, regions: I)
-                                          -> Result<UnsafeCommandBufferBuilder, BufferCopyError>
+    pub fn new<Bs, Bd, I>(src: &Arc<Bs>, dest: &Arc<Bd>, regions: I)
+                          -> Result<BufferCopyCommand, BufferCopyError>
         where Bs: Buffer + Send + Sync + 'static,
               Bd: Buffer + Send + Sync + 'static,
               I: IntoIterator<Item = BufferCopyRegion>
     {
-        unsafe {
-            // Various safety checks.
-            if self.within_render_pass { return Err(BufferCopyError::ForbiddenWithinRenderPass); }
-            assert_eq!(src.inner_buffer().device().internal_object(),
-                       self.pool.device().internal_object());
-            assert_eq!(dest.inner_buffer().device().internal_object(),
-                       self.pool.device().internal_object());
-            if !src.inner_buffer().usage_transfer_src() ||
-               !dest.inner_buffer().usage_transfer_dest()
-            {
-                return Err(BufferCopyError::WrongUsageFlag);
-            }
+        // Various checks.
+        assert_eq!(src.inner_buffer().device().internal_object(),
+                   dest.inner_buffer().device().internal_object());
+        if !src.inner_buffer().usage_transfer_src() ||
+           !dest.inner_buffer().usage_transfer_dest()
+        {
+            return Err(BufferCopyError::WrongUsageFlag);
+        }
 
-            // Building the list of regions.
-            let regions: SmallVec<[_; 4]> = {
-                let mut res = SmallVec::new();
-                for region in regions.into_iter() {
-                    if region.source_offset + region.size > src.size() {
-                        return Err(BufferCopyError::OutOfRange);
-                    }
-                    if region.destination_offset + region.size > dest.size() {
-                        return Err(BufferCopyError::OutOfRange);
-                    }
-                    if region.size == 0 { continue; }
-
-                    res.push(vk::BufferCopy {
-                        srcOffset: region.source_offset as vk::DeviceSize,
-                        dstOffset: region.destination_offset as vk::DeviceSize,
-                        size: region.size as vk::DeviceSize,
-                    });
+        // Building the list of regions.
+        let regions: SmallVec<[_; 4]> = {
+            let mut res = SmallVec::new();
+            for region in regions.into_iter() {
+                if region.source_offset + region.size > src.size() {
+                    return Err(BufferCopyError::OutOfRange);
                 }
-                res
-            };
+                if region.destination_offset + region.size > dest.size() {
+                    return Err(BufferCopyError::OutOfRange);
+                }
+                if region.size == 0 { continue; }
+
+                res.push(vk::BufferCopy {
+                    srcOffset: region.source_offset as vk::DeviceSize,
+                    dstOffset: region.destination_offset as vk::DeviceSize,
+                    size: region.size as vk::DeviceSize,
+                });
+            }
+            res
+        };
+
+        // Checking for overlaps.
+        for r1 in 0 .. regions.len() {
+            for r2 in (r1 + 1) .. regions.len() {
+                let r1 = &regions[r1];
+                let r2 = &regions[r2];
+
+                if r1.srcOffset <= r2.srcOffset && r1.srcOffset + r1.size >= r2.srcOffset {
+                    return Err(BufferCopyError::OverlappingRegions);
+                }
+                if r2.srcOffset <= r1.srcOffset && r2.srcOffset + r2.size >= r1.srcOffset {
+                    return Err(BufferCopyError::OverlappingRegions);
+                }
+                if r1.dstOffset <= r2.dstOffset && r1.dstOffset + r1.size >= r2.dstOffset {
+                    return Err(BufferCopyError::OverlappingRegions);
+                }
+                if r2.dstOffset <= r1.dstOffset && r2.dstOffset + r2.size >= r1.dstOffset {
+                    return Err(BufferCopyError::OverlappingRegions);
+                }
+
+                if src.inner_buffer().internal_object() == dest.inner_buffer().internal_object() {
+                    if r1.srcOffset <= r2.dstOffset && r1.srcOffset + r1.size >= r2.dstOffset {
+                        return Err(BufferCopyError::OverlappingRegions);
+                    }
+                    if r2.srcOffset <= r1.dstOffset && r2.srcOffset + r2.size >= r1.dstOffset {
+                        return Err(BufferCopyError::OverlappingRegions);
+                    }
+                }
+            }
+        }
+
+        Ok(BufferCopyCommand {
+            internal_src: src.inner_buffer().internal_object(),
+            internal_dest: dest.inner_buffer().internal_object(),
+            device: src.inner_buffer().device().internal_object(),
+            keep_alive_src: src.clone(),
+            keep_alive_dest: dest.clone(),
+            regions: regions,
+        })
+    }
+
+    /// Submits the command to the command buffer.
+    ///
+    /// # Panic
+    ///
+    /// - Panicks if the command buffer is within a render pass.
+    /// - Panicks if the buffers were not allocated with the same device as the command buffer.
+    ///
+    pub fn submit(self, mut cb: UnsafeCommandBufferBuilder) -> UnsafeCommandBufferBuilder {
+        unsafe {
+            // Various checks.
+            assert!(!cb.within_render_pass);
+            assert_eq!(self.device, cb.pool.device().internal_object());
 
             // Vulkan requires that the number of regions must always be >= 1.
-            if regions.is_empty() { return Ok(self); }
+            if self.regions.is_empty() { return cb; }
 
-            // Checking for overlaps.
-            for r1 in 0 .. regions.len() {
-                for r2 in (r1 + 1) .. regions.len() {
-                    let r1 = &regions[r1];
-                    let r2 = &regions[r2];
-
-                    if r1.srcOffset <= r2.srcOffset && r1.srcOffset + r1.size >= r2.srcOffset {
-                        return Err(BufferCopyError::OverlappingRegions);
-                    }
-                    if r2.srcOffset <= r1.srcOffset && r2.srcOffset + r2.size >= r1.srcOffset {
-                        return Err(BufferCopyError::OverlappingRegions);
-                    }
-                    if r1.dstOffset <= r2.dstOffset && r1.dstOffset + r1.size >= r2.dstOffset {
-                        return Err(BufferCopyError::OverlappingRegions);
-                    }
-                    if r2.dstOffset <= r1.dstOffset && r2.dstOffset + r2.size >= r1.dstOffset {
-                        return Err(BufferCopyError::OverlappingRegions);
-                    }
-
-                    if src.inner_buffer().internal_object() ==
-                       dest.inner_buffer().internal_object()
-                    {
-                        if r1.srcOffset <= r2.dstOffset && r1.srcOffset + r1.size >= r2.dstOffset {
-                            return Err(BufferCopyError::OverlappingRegions);
-                        }
-                        if r2.srcOffset <= r1.dstOffset && r2.srcOffset + r2.size >= r1.dstOffset {
-                            return Err(BufferCopyError::OverlappingRegions);
-                        }
-                    }
-                }
-            }
-
-            // Now adding the command.
-            self.keep_alive.push(src.clone());
-            self.keep_alive.push(dest.clone());
+            cb.keep_alive.push(self.keep_alive_src);
+            cb.keep_alive.push(self.keep_alive_dest);
 
             {
-                let vk = self.device.pointers();
-                let cmd = self.cmd.clone().unwrap();
-                vk.CmdCopyBuffer(cmd, src.inner_buffer().internal_object(),
-                                 dest.inner_buffer().internal_object(), regions.len() as u32,
-                                 regions.as_ptr());
+                let vk = cb.device.pointers();
+                let cmd = cb.cmd.clone().unwrap();
+                vk.CmdCopyBuffer(cmd, self.internal_src, self.internal_dest,
+                                 self.regions.len() as u32, self.regions.as_ptr());
             }
 
-            Ok(self)
+            cb
         }
     }
 }
@@ -181,7 +206,6 @@ pub struct BufferCopyRegion {
 }
 
 error_ty!{BufferCopyError => "Error that can happen when copying between buffers.",
-    ForbiddenWithinRenderPass => "can't copy buffers from within a render pass",
     OutOfRange => "one of regions is out of range of the buffer",
     WrongUsageFlag => "one of the buffers doesn't have the correct usage flag",
     OverlappingRegions => "some regions are overlapping",
